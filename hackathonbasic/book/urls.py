595531b99e028354d7a86ad13cc253915176800b
hosomatu <kcfdmori0759@gmail.com>
2024-12-21 20:55:13 +0900

    bootstrapの導入

--- hackathonbasic/book/urls.py
+++ hackathonbasic/book/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+# path()関数。第一引数がurlパス。第二引数がビュー関数など、マッチしたら呼び出されるもの。
 urlpatterns = [
     path('book/', views.ListBookView.as_view()),
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view()), #プライマリーキー(id)
 ]
--- hackathonbasic/book/views.py
+++ hackathonbasic/book/views.py
@@ -1,8 +1,12 @@
 # Viewクラスを定義。表示するテンプレートと使用するモデルを定義
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 class ListBookView(ListView) :
     template_name = 'book/book_list.html' # テンプレートのパスを指定
     model = Book # 使用するモデルの指定
+
+class DetailBookView(DetailView) :
+    template_name = 'book/book_detail.html'
+    model = Book
