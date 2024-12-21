e361975552c292a40b11e0af762f9ae47e7596fd
hosomatu <kcfdmori0759@gmail.com>
2024-12-21 23:21:54 +0900

    create機能の追加

--- hackathonbasic/book/urls.py
+++ hackathonbasic/book/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 # path()関数。第一引数がurlパス。第二引数がビュー関数など、マッチしたら呼び出されるもの。
 urlpatterns = [
-    path('book/', views.ListBookView.as_view()),
-    path('book/<int:pk>/detail/', views.DetailBookView.as_view()), #プライマリーキー(id)
+    path('book/', views.ListBookView.as_view(), name='list-book'),
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'), #プライマリーキー(id)
+    path('book/create/', views.CreateBookView.as_view(), name='create-book'),
 ]
--- hackathonbasic/book/views.py
+++ hackathonbasic/book/views.py
@@ -1,6 +1,9 @@
 # Viewクラスを定義。表示するテンプレートと使用するモデルを定義
+# railsでいうコントローラー
+
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Book
 
 class ListBookView(ListView) :
@@ -10,3 +13,9 @@ class ListBookView(ListView) :
 class DetailBookView(DetailView) :
     template_name = 'book/book_detail.html'
     model = Book
+
+class CreateBookView(CreateView) :
+    template_name = 'book/book_create.html'
+    model = Book
+    fields = ('title', 'text', 'category')
+    success_url = reverse_lazy('list-book') # reverce逆の動作。つまり普段はurlからviewsの名前を得ているがそれの逆(viewの名前からurlを得る)を行う。lazyによりDjangoの初期化のタイミングで処理が走らないように送らせている。
