2215e4d02fbdcec51291b6ffb6d5290c1852e614
hosomatu <kcfdmori0759@gmail.com>
2024-12-21 23:32:43 +0900

    削除機能の実装

--- hackathonbasic/book/urls.py
+++ hackathonbasic/book/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from . import views
 
-# path()関数。第一引数がurlパス。第二引数がビュー関数など、マッチしたら呼び出されるもの。
+# path()関数。第一引数がurlパス。第二引数がビュー関数など、マッチしたら呼び出されるもの。第3引数はURLに名前をつけている。
 urlpatterns = [
     path('book/', views.ListBookView.as_view(), name='list-book'),
-    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'), #プライマリーキー(id)
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'), #整数型のプライマリーキー(id)
     path('book/create/', views.CreateBookView.as_view(), name='create-book'),
+    path('book/<int:pk>/delete', views.DeleteBookView.as_view(), name='delete-book'),
 ]
--- hackathonbasic/book/views.py
+++ hackathonbasic/book/views.py
@@ -3,10 +3,10 @@
 
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Book
 
-class ListBookView(ListView) :
+class ListBookView(ListView) : #Pythonでは()の部分が継承する親クラス
     template_name = 'book/book_list.html' # テンプレートのパスを指定
     model = Book # 使用するモデルの指定
 
@@ -19,3 +19,8 @@ class CreateBookView(CreateView) :
     model = Book
     fields = ('title', 'text', 'category')
     success_url = reverse_lazy('list-book') # reverce逆の動作。つまり普段はurlからviewsの名前を得ているがそれの逆(viewの名前からurlを得る)を行う。lazyによりDjangoの初期化のタイミングで処理が走らないように送らせている。
+
+class DeleteBookView(DeleteView) :
+    template_name = 'book/book_confirm_delete.html'
+    model = Book
+    success_url = reverse_lazy('list-book')
