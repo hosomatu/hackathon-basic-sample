fe17fa855fb775e3d52c009f9da83fcb3fed6075
hosomatu <kcfdmori0759@gmail.com>
2024-12-31 13:57:25 +0900

    ログインしていなければ一部のページにアクセスできないように修正

--- hackathonbasic/book/views.py
+++ hackathonbasic/book/views.py
@@ -6,27 +6,28 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Book, Review
+from django.contrib.auth.mixins import LoginRequiredMixin # DjangoのMixinは、Djangoのクラスベースビュー（CBV）やモデルでコードの再利用性を高めるための設計パターン
 
-class ListBookView(ListView) : #Pythonでは()の部分が継承する親クラス
+class ListBookView(LoginRequiredMixin, ListView) : #Pythonでは()の部分が継承する親クラス。LoginRequiredMixinによってログインしていない状態では表示しないようになる。ログインしていない場合はデフォルトでaccounts/loginに飛ばされる(settings.pyのLOGIN_URLで調整可能)
     template_name = 'book/book_list.html' # テンプレートのパスを指定
     model = Book # 使用するモデルの指定
 
-class DetailBookView(DetailView) :
+class DetailBookView(LoginRequiredMixin, DetailView) :
     template_name = 'book/book_detail.html'
     model = Book
 
-class CreateBookView(CreateView) :
+class CreateBookView(LoginRequiredMixin, CreateView) :
     template_name = 'book/book_create.html'
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail') # フォームの内容
     success_url = reverse_lazy('list-book') # reverce逆の動作。つまり普段はurlからviewsの名前を得ているがそれの逆(viewの名前からurlを得る)を行う。lazyによりDjangoの初期化のタイミングで処理が走らないように送らせている。
 
-class DeleteBookView(DeleteView) :
+class DeleteBookView(LoginRequiredMixin, DeleteView) :
     template_name = 'book/book_confirm_delete.html'
     model = Book
     success_url = reverse_lazy('list-book')
 
-class UpdateBookView(UpdateView) :
+class UpdateBookView(LoginRequiredMixin, UpdateView) :
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail')
     template_name = 'book/book_update.html'
@@ -40,7 +41,7 @@ def index_view(request) :
     # 第三引数でテンプレートに渡すコンテキスト（辞書形式のデータ)先に定義したobject_list(右)を'object_list'(左)という名前で呼び出せるようにしている
     return render(request, 'book/index.html', {'object_list': object_list})
 
-class CreateReview(CreateView) :
+class CreateReview(LoginRequiredMixin, CreateView) :
     model = Review
     fields = ('book', 'title', 'text', 'rate')
     template_name = 'book/review_form.html'
